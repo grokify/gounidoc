be767d7b28bd15202f94e5b06675e86a8ed98b11
John Wang <johncwang@gmail.com>
2025-01-24 21:30:14 -0800

    feat `cmd/docx2pdf`: init

---
+++ cmd/docx2pdf/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+
+	"github.com/grokify/gounidoc"
+	"github.com/grokify/gounidoc/uniofficeutil"
+	"github.com/grokify/mogo/errors/errorsutil"
+	"github.com/grokify/mogo/log/logutil"
+	"github.com/jessevdk/go-flags"
+)
+
+type Options struct {
+	InputFile  string `short:"d" long:"delete" description:"Delete subscription"`
+	OutputFile string `short:"r" long:"recreate" description:"Recreate subscription"`
+}
+
+func main() {
+	opts := Options{}
+	_, err := flags.Parse(&opts)
+	logutil.FatalErr(err)
+
+	err = gounidoc.SetMeteredKeyEnv()
+	logutil.FatalErr(err)
+
+	if opts.OutputFile == "" && opts.InputFile != "" {
+		opts.OutputFile = opts.InputFile + ".pdf"
+	}
+
+	err = uniofficeutil.DOCXToPDF(opts.InputFile, opts.OutputFile)
+	if err != nil {
+		slog.Error(errorsutil.NewErrorWithLocation(err.Error()).Error())
+		logutil.FatalErr(err)
+	}
+
+	fmt.Println("DONE")
+}
